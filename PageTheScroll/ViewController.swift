fcac2b797b664f5ab33efef7f05e1fcb96cd1273
mik3ycodez <mjharris0114@gmail.com>
2017-04-29 13:07:11 -0700

    Code cleanup
    Removed some unused commented code

--- PageTheScroll/ViewController.swift
+++ PageTheScroll/ViewController.swift
@@ -38,7 +38,7 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         
         var contentWidth: CGFloat = 0.0
         let scrollWidth = scrollView.frame.size.width
-//        let scrollHeight = scrollView.frame.size.height
+        let scrollHeight = scrollView.frame.size.height
         
         for x in 0...2 {
             let image = UIImage(named: "icon\(x).png")
@@ -47,17 +47,14 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
             
             var newX: CGFloat = 0.0
             newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
-            
-            //contentWidth += newX
             contentWidth += scrollWidth * CGFloat(x)
             
             scrollView.addSubview(imageView)
             
-            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
+            imageView.frame = CGRect(x: newX - 75, y: (scrollHeight / 2) - 75, width: 150, height: 150)
             
         }
         
-//        scrollView.backgroundColor = UIColor.purple
         scrollView.clipsToBounds = false
         scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
     }
@@ -67,7 +64,7 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         if let swipeGesture = sender as? UISwipeGestureRecognizer {
             switch swipeGesture.direction {
             case UISwipeGestureRecognizerDirection.left:
-                print ("Swiped left!")
+                print("Swiped left!")
                 if currentPage < MAX_PAGE { moveScrollView(direction: 1) }
                 
             case UISwipeGestureRecognizerDirection.right:
@@ -85,11 +82,6 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         scrollView.setContentOffset(point, animated: true)
         
     }
-
-    
-//    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-//        return true
-//    }
 }
 
 
